28f545c2b04ced7aeb038e47ad6eb4c1e58deee5
Tim Bedin <captainceramic@gmail.com>
2014-10-29 15:57:36 +1100

    Fixed up some tempfile problems with this script

---
+++ indices/ENSO_months.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+##############################################################################
+#
+# Description: Calculate the Niño 3.4 index from an
+#              input netCDF monthly time series.
+#              
+# Modules Required: cct, cdo
+#
+# Authors:     Tim Bedin
+#              Michael Grose
+#
+# Copyright:   2014 CSIRO
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#############################################################################
+
+temp_dir=$(mktemp -d)
+function cleanup {
+    rm -rf $temp_dir
+}
+trap cleanup EXIT
+
+usage () {
+    echo "Calculate the Nino3.4 index from an input sst file."
+    echo "$0 infile outfile startdate enddate"
+    echo 
+    echo "outputs a montly time series of NINO3.4 index between dates specified"
+    echo
+    echo "Requires the cawcr cct module."
+}
+
+# Checks inputs are correct
+if [ $# -lt 4 ]; then
+    usage
+    exit 1
+fi
+
+sstfile=$1
+outputfile=$2
+startdate=$3
+enddate=$4
+
+# Collate the model data into one big file to work with
+inbase=$(basename $sstfile)
+extn=$(expr match "${inbase}" '.*\.\(.*\)')
+if [ $extn = 'xml' ] ; then
+    tmp_in=$temp_dir/xml_concat.$$.nc
+    python $CCT/processing/cdml_cat/xml_to_nc.py None $sstfile $tmp_in
+    sstfile=$tmp_in
+fi
+
+# Define tempfile locations.
+tempinput=$temp_dir/restrictedtime.$$.nc
+temp1=$temp_dir/clim_temp.$$.nc
+temp2=$temp_dir/time_series_temp.$$.nc
+
+# restrict input to required year dates
+cdo seldate,$startdate"-1-1",$enddate"-31-12" $sstfile $tempinput
+
+# Check years present in the input file
+years=$(cdo showyear ${tempinput})
+
+# isolate the decades in the source file, make an array of the decades
+IFS=' ' read -a yeararray <<< "$years"
+
+for year in "${yeararray[@]}"; do
+    if [ $(( ${year} % 10 )) -eq 0 ]; then
+        decades="$decades $year"
+    fi
+done
+
+IFS=' ' read -a decadearray <<< "$decades"
+num_decades=${#decadearray[@]}
+
+# Calculate climatology and anomalies for first decade, using first 3 decades as climatology
+cyr1=$((${decadearray[0]}+1))
+cyr2=$((${decadearray[0]}+30))
+year1=$((cyr1-1))
+year2=$((cyr1+8))
+echo "cyr1 = $cyr1 cyr2 = $cyr2 year1 = $year1 year2 = $year2"
+cdo ymonmean -seldate,$cyr1"-1-1",$cyr2"-31-12" $tempinput $temp1
+cdo ymonsub -seldate,$year1"-1-1",$year2"-31-12" $tempinput $temp1 $temp2
+cdo fldavg -sellonlatbox,190,240,-5,5 $temp2 $temp_dir/temp_$$_n34_${year1}-${year2}.nc
+
+# loop through all decades except 1 and last two, make anomaly then calculate NINO3.4 index
+for dcd in "${decadearray[@]:1:$((num_decades - 3))}"; do
+    cyr1=$((dcd-9))
+    cyr2=$((dcd+20))
+    year1=$dcd
+    year2=$((dcd+9))
+    echo "cyr1 = $cyr1 cyr2 = $cyr2 year1 = $year1 year2 = $year2"
+    cdo ymonmean -seldate,$cyr1"-1-1",$cyr2"-31-12" $tempinput $temp1
+    cdo ymonsub -seldate,$year1"-1-1",$year2"-31-12" $tempinput $temp1 $temp2
+    # Calculate NINO3.4
+    cdo fldavg -sellonlatbox,190,240,-5,5 $temp2 $temp_dir/temp_$$_n34_${year1}-${year2}.nc
+done
+
+# Calculate climatology and anomalies for second last decade, using last 3 decades as climatology
+cyr1=$((${decadearray[num_decades-4]}+1))
+cyr2=$((${decadearray[num_decades-1]}))
+cdo ymonmean -seldate,$cyr1"-1-1",$cyr2"-31-12" $tempinput $temp1
+year1=$((cyr2-10))
+year2=$((cyr2-1))
+echo "cyr1 = $cyr1 cyr2 = $cyr2 year1 = $year1 year2 = $year2"
+cdo ymonsub -seldate,$year1"-1-1",$year2"-31-12" $tempinput $temp1 $temp2
+# Calculate NINO3.4
+cdo fldavg -sellonlatbox,190,240,-5,5 $temp2 $temp_dir/temp_$$_n34_${year1}-${year2}.nc
+
+# Put all NINO3.4 files together and save to output file
+cdo -O mergetime $temp_dir/temp_$$_n34_????-????.nc $outputfile
+
+# Check time parameters
+yearin=$(cdo showyear $tempinput)
+yearout=$(cdo showyear $outputfile)
+echo "years in = $yearin years out= $yearout"
+
