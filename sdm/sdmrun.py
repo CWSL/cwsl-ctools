2a199055e785ef72c5e08808e0e1b8e05c2b5795
Yang Wang <y.wang@bom.gov.au>
2015-05-04 10:41:57 +1000

    Added dxt-gridded2 for taking regular arguments in addition to cod path.

--- sdm/sdmrun.py
+++ sdm/sdmrun.py
@@ -33,7 +33,7 @@ def main(args):
 
     ap.add_argument('-c', '--config-file',
                     required=False,
-                    help='The configuration file, default to "$HOME/.sdm.cfg"')
+                    help='the configuration file, default to "$HOME/.sdm.cfg"')
     ap.add_argument('-V', '--verbose',
                     action='store_true',
                     default=False,
@@ -67,14 +67,37 @@ def main(args):
                                     help='predictand name, e.g. rain, tmax, tmin')
 
     dxt_gridded_parser = subparsers.add_parser('dxt-gridded',
-                                               help='extract gridded data with the given cod file')
+                                               help='extract gridded data using the given cod file')
     dxt_gridded_parser.add_argument('output_file',
                                     help='output netCDF file name')
     dxt_gridded_parser.add_argument('cod_file_path',
                                     help='full path to the CoD file')
-    dxt_gridded_parser.add_argument('-r', '--region',
+    dxt_gridded_parser.add_argument('-R', '--region',
                                     required=False,
                                     help='the region where the data are to be extracted')
+
+    dxt_gridded2_parser = subparsers.add_parser('dxt-gridded2',
+                                                help='extract gridded data with the given parameters')
+    dxt_gridded2_parser.add_argument('output_file',
+                                     help='output netCDF file name')
+    dxt_gridded2_parser.add_argument('-m', '--model',
+                                     required=True,
+                                     help='model name')
+    dxt_gridded2_parser.add_argument('-c', '--scenario',
+                                     required=False,
+                                     help='scenario name, e.g. historical, rcp45, rcp85')
+    dxt_gridded2_parser.add_argument('-r', '--region-type',
+                                     required=True,
+                                     help='pre-defined region type name, e.g. sea, sec, tas ...')
+    dxt_gridded2_parser.add_argument('-s', '--season',
+                                     required=True,
+                                     help='season number, e.g. 1 (DJF), 2 (MAM), 3 (JJA), or 4 (SON)')
+    dxt_gridded2_parser.add_argument('-p', '--predictand',
+                                     required=True,
+                                     help='predictand name, e.g. rain, tmax, tmin')
+    dxt_gridded2_parser.add_argument('-R', '--region',
+                                     required=False,
+                                     help='the region where the data are to be extracted (default to region-type)')
 
     ns = ap.parse_args(args)
 
@@ -84,13 +107,18 @@ def main(args):
         print CoD(config.get('dxt', 'cod_base_dir'), verbose=ns.verbose).get_cod_file_path(
             ns.model, ns.scenario, ns.region_type, ns.season, ns.predictand)
 
-    elif ns.sub_command == 'dxt-gridded':
+    elif ns.sub_command in ('dxt-gridded', 'dxt-gridded2'):
         gridded_extractor = GriddedExtractor(cod_base_dir=config.get('dxt', 'cod_base_dir'),
                                              mask_base_dir=config.get('dxt', 'mask_base_dir'),
                                              gridded_base_dir=config.get('dxt', 'gridded_base_dir'),
                                              verbose=ns.verbose)
 
-        model, scenario, region_type, season, predictand = CoD.get_components_from_path(ns.cod_file_path)
+        if ns.sub_command == 'dxt-gridded':
+            model, scenario, region_type, season, predictand = CoD.get_components_from_path(ns.cod_file_path)
+        else:
+            model, scenario, region_type, season, predictand = \
+                ns.model, ns.scenario, ns.region_type, ns.season, ns.predictand
+
         data, dates, lat, lon = gridded_extractor.extract(model, scenario, region_type, season, predictand,
                                                           ns.region)
         GriddedExtractor.save_netcdf(ns.output_file, data, dates, lat, lon,
