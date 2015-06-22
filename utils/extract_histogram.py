90ae15792b20ba1d1642f5d91b5115ca467fd3e0
captainceramic <captainceramic@gmail.com>
2015-06-22 11:04:07 +1000

    Added the time bounds to the histogram JSON response.

--- utils/extract_histogram.py
+++ utils/extract_histogram.py
@@ -9,6 +9,7 @@ corresponding density.
 import argparse
 import json
 import sys
+import datetime as dt
 
 import numpy as np
 import scipy.io.netcdf as nc
@@ -23,6 +24,28 @@ def main(args):
 
     # Grab the variable
     input_var = input_file.variables[args.varname]
+
+    # Extract the start and end times
+    time_var = input_file.variables["time"]
+
+    # This is a little brittle - this extraction makes an
+    # assumption about the use of a standard calendar.
+    units_re = r"^(?P<unit>\S+) since (?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+).+$"
+
+    match_dict = re.match(units_re, time_var.units).groupdict()
+    start_date = dt.date(int(match_dict["year"]), int(match_dict["month"]),
+                         int(match_dict["day"]))
+
+    if match_dict["unit"] != "days":
+        raise Exception("Time unit: {} not understood"
+                        .format(match_dict["unit"]))
+    if time_var.calendar != "standard":
+        print("WARNING: calendar {} is not standard. JSON date output may be incorrect"
+              .format(time_var.calendar))
+
+    start_time = start_date + dt.timedelta(days=int(time_var[0]))
+    end_time = start_date + dt.timedelta(days=int(time_var[0]))
+    time_bounds = [start_time.isoformat(), end_time.isoformat()]
 
     # Test that the required x and y fits.
     val_shape = input_var.shape
@@ -40,7 +63,7 @@ def main(args):
 
     # Use the numpy histogram calculator.
     counts, bins = np.histogram(output_data, bins=args.bins)
-    
+
     # Write it out using json.dumps
     # we write out the count in each bin, the
     # bins and the total number of entries.
@@ -50,8 +73,9 @@ def main(args):
 
     output = {"bins": outbins,
               "counts": counts.tolist(),
-              "num_entries": len(output_data)}
-    
+              "num_entries": len(output_data),
+              "time_bounds": time_bounds}
+
     with open(args.outfile, 'w') as output_file:
         output_file.write(json.dumps(output))
 
