b2dc31855ce5281cfb79cbe97ce29fa8a24ba8bd
captainceramic <captainceramic@gmail.com>
2015-08-31 15:29:35 +1000

    Alternative version that does time aggregation first, including changes from Duan.

--- indices/nino34_onestep.sh
+++ indices/nino34_onestep.sh
@@ -24,26 +24,21 @@ fi
 time_temp=$(mktemp -t nino_timetemp$$.XXXXXX.nc)
 merged_temp=$(mktemp -t nino_mergetemp$$.XXXXXX.nc)
 anomaly_temp=$(mktemp -t nino_anomalytemp$$.XXXXXX.nc)
-offset_temp=$(mktemp -t nino_offsettemp$$.XXXXXX.nc)
-slope_temp=$(mktemp -t nino_slopetemp$$.XXXXXX.nc)
-detrended_temp=$(mktemp -t nino_detrendedtemp$$.XXXXXX.nc)
-detrended_anomaly_temp=$(mktemp -t nino_detrendedanomalytemp$$.XXXXXX.nc)
 function cleanup() {
-    rm "$merged_temp" "$anomaly_temp" "$offset_temp" "$slope_temp"  "$detrended_temp" "$detrended_anomaly_temp" "$time_temp"
+    rm "$merged_temp" "$anomaly_temp" "$time_temp"
 }
 trap cleanup EXIT
 
-# Merge the timeseries, then select the correct dates.
-cdo -s -r -mergetime ${infilenames} ${merged_temp}
-cdo -s -r -selyear,1900/2100 ${merged_temp} ${time_temp}
+# If infilenames contains spaces then must be more than one file.
+if [[ $infilenames == *" "* ]] ; then
+    cdo -s mergetime ${infilenames} ${merged_temp}
+    cdo -s -r -fldmean -sellonlatbox,190,240,-5,5 -selyear,1900/2100 ${merged_temp} ${time_temp}
+else
+    cdo -s -r -fldmean -sellonlatbox,190,240,-5,5 -selyear,1900/2100 ${infilenames} ${time_temp}
+fi
 
 # Calculate and subtract monthly climatology to make anomaly time series
-cdo -s -r -ymonsub ${merged_temp} -ymonmean ${time_temp} ${anomaly_temp}
-
-# Subtract the linear trend, but keep the offset.
-cdo -s -r trend ${anomaly_temp} ${offset_temp} ${slope_temp}
-cdo -s -r detrend ${anomaly_temp} ${detrended_temp}
-cdo -s add ${detrended_temp} ${offset_temp} ${detrended_anomaly_temp}
+cdo -s -r -ymonsub ${time_temp} -ymonmean ${time_temp} ${anomaly_temp}
 
 # Detrend and take the field mean over the Nino3.4 box.
-cdo -s -fldmean -sellonlatbox,190,240,-5,5 ${detrended_anomaly_temp} ${outfilename}
+cdo -s detrend ${anomaly_temp} ${outfilename}
